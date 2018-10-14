da05b3d1cb953d2ab2e9776b33acbc604442488d
Christopher Salinas <salinas23cfh@gmail.com>
2018-10-13 22:09:40 -0500

    working on Jasmine test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,11 +32,27 @@ $(function() {
          * and that the URL is not empty.
          */
 
+        it('url defined', function () {
+            for(let feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            }
+
+        });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        
+        it('name defined', function () {
+            for(let feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+        });
+
     });
 
 
